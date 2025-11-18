43705bb9bdb8319bebbdb4fc037e3f8dd9b0d4b5
StormKroyer <stormkroyer@icloud.com>
2025-11-18 14:25:37 +0100

    fixed enemy movement

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -6,22 +7,32 @@ public class Enemy : MonoBehaviour
     private Transform target;
     public float speed = 5;
     Rigidbody2D rb;
+    public Boolean right = false;
+    public Boolean left = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
+        if (transform.position.x > target.position.x)
+        {
+            right = true;
+        }
+        if (transform.position.x < target.position.x)
+        {
+            left = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x > target.position.x) 
+        if (right == true) 
         {
             rb.linearVelocity = new Vector2(-speed, rb.linearVelocity.y);
         }
 
-        if (transform.position.x < target.position.x) 
+        if (left == true) 
         {
             rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
         }
